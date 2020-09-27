685a13c0cdc0f729b0a495cfa26b32a2b3c5dd5b
ghLiYuan <48039674+ghLiYuan@users.noreply.github.com>
2020-09-27 16:33:48 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,28 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.HTMLButton")
+                .setHost(host,"xui_ui_htmlbutton3")
+                .setLeft("21.333333333333332em")
+                .setTop("19.047619047619047em")
+                .setCaption("Native Button")
+                .onClick([
+                    {
+                        "desc":"Action 1",
+                        "type":"other",
+                        "target":"msg",
+                        "args":[
+                            "say",
+                            "hello"
+                        ],
+                        "method":"pop",
+                        "event":1
+                    }
+                ])
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
